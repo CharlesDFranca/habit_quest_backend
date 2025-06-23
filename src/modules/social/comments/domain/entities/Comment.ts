4400098048590a2c4d45454880ca8ee5f197de79
CharlesDFranca <josecharlestill@gmail.com>
2025-06-22 23:53:57 -0300

    creation of the 'Comment' entity and its tests

---
+++ src/modules/social/comments/domain/entities/Comment.ts
@@ -0,0 +1,111 @@
+import { Entity } from "@/shared/domain/entities/Entity";
+import { Id } from "@/shared/domain/value-objects/Id";
+
+type CommentProps = {
+  authorId: Id<"UserId">;
+  postId: Id<"PostId">;
+  content: string;
+  replyIds: Id<"CommentId">[];
+  likeIds: Id<"LikeId">[];
+  createdAt?: Date;
+  updatedAt?: Date;
+};
+
+export class Comment extends Entity<"CommentId"> {
+  private constructor(
+    private readonly commentId: Id<"CommentId">,
+    private readonly props: CommentProps,
+  ) {
+    super(commentId, props.createdAt, props.updatedAt);
+  }
+
+  static create(props: CommentProps, _id?: Id<"CommentId">): Comment {
+    const id = _id ?? Id.generate<"CommentId">();
+    return new Comment(id, props);
+  }
+
+  //#region getter methods
+  get authorId(): Id<"UserId"> {
+    return this.props.authorId;
+  }
+
+  get postId(): Id<"PostId"> {
+    return this.props.postId;
+  }
+
+  get content(): string {
+    return this.props.content;
+  }
+
+  get replyIds(): Id<"CommentId">[] {
+    return [...this.props.replyIds];
+  }
+
+  get likeIds(): Id<"LikeId">[] {
+    return [...this.props.likeIds];
+  }
+  //#endregion
+
+  //#region update something methods
+  updateContent(content: string): void {
+    this.props.content = content;
+    this.touch();
+  }
+
+  private updateLikeIds(likeIds: Id<"LikeId">[]): void {
+    this.props.likeIds = likeIds;
+    this.touch();
+  }
+
+  private updateReplyIds(replyIds: Id<"CommentId">[]): void {
+    this.props.replyIds = replyIds;
+    this.touch();
+  }
+  //#endregion
+
+  //#region add and remove something methods
+  addLikeId(likeId: Id<"LikeId">): void {
+    const alreadLike = this.props.likeIds.some((commentLikeId) =>
+      commentLikeId.isEqual(likeId),
+    );
+
+    if (alreadLike) return;
+
+    const newLikeIds = [...this.props.likeIds, likeId];
+
+    this.updateLikeIds(newLikeIds);
+  }
+
+  removeLikeId(likeId: Id<"LikeId">): void {
+    if (!this.props.likeIds.includes(likeId)) return;
+
+    const newLikeIds = this.props.likeIds.filter(
+      (commentLikeId) => !commentLikeId.isEqual(likeId),
+    );
+
+    this.updateLikeIds(newLikeIds);
+  }
+
+  addReplyId(replyId: Id<"CommentId">): void {
+    const alreadyReplied = this.props.replyIds.some((commentReplyId) =>
+      commentReplyId.isEqual(replyId),
+    );
+
+    if (alreadyReplied) return;
+
+    const newReplyIds = [...this.props.replyIds, replyId];
+
+    this.updateReplyIds(newReplyIds);
+  }
+
+  removeReplyId(replyId: Id<"CommentId">): void {
+    if (!this.props.replyIds.includes(replyId)) return;
+
+    const newReplyIds = this.props.replyIds.filter(
+      (commentReplyId) => !commentReplyId.isEqual(replyId),
+    );
+
+    this.updateReplyIds(newReplyIds);
+  }
+  //#endregion
+}
---
+++ tests/unit/modules/social/comments/domain/entities/Comment.spec.ts
@@ -0,0 +1,204 @@
+import { Comment } from "@/modules/social/comments/domain/entities/Comment";
+import { Id } from "@/shared/domain/value-objects/Id";
+import { describe, it, expect, vi, beforeEach, afterEach } from "vitest";
+
+const makeComment = (createdAt?: Date, updatedAt?: Date) =>
+  Comment.create({
+    authorId: Id.generate<"UserId">(),
+    postId: Id.generate<"PostId">(),
+    likeIds: [],
+    replyIds: [],
+    content: "Comment content",
+    createdAt: createdAt ?? new Date(),
+    updatedAt: updatedAt ?? createdAt ?? new Date(),
+  });
+
+describe("Comment entity unit tests", () => {
+  let sut: Comment;
+
+  beforeEach(() => {
+    sut = makeComment();
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+
+  describe("getters", () => {
+    it("should expose getters correctly", () => {
+      expect(sut.id).toBeInstanceOf(Id);
+      expect(sut.authorId).toBeInstanceOf(Id);
+      expect(sut.postId).toBeInstanceOf(Id);
+      expect(sut.likeIds).toBeInstanceOf(Array);
+      expect(sut.replyIds).toBeInstanceOf(Array);
+      expect(sut.content).toBe("Comment content");
+    });
+
+    it("should return likeIds safely (immutable array)", () => {
+      const likes = sut.likeIds;
+
+      expect(Array.isArray(likes)).toBeTruthy();
+
+      likes.push(Id.generate<"LikeId">());
+
+      expect(sut.likeIds.length).toBe(0);
+    });
+
+    it("should return replyIds safely (immutable array)", () => {
+      const replys = sut.replyIds;
+
+      expect(Array.isArray(replys)).toBeTruthy();
+
+      replys.push(Id.generate<"CommentId">());
+
+      expect(sut.likeIds.length).toBe(0);
+    });
+  });
+
+  describe("Content", () => {
+    it("should update content and touch updatedAt", () => {
+      const oldUpdatedAt = sut.updatedAt;
+
+      vi.advanceTimersByTime(10);
+
+      const updatedContent = "Updated content";
+
+      sut.updateContent(updatedContent);
+
+      expect(sut.content).toBe(updatedContent);
+      expect(sut.updatedAt.getTime()).toBeGreaterThan(oldUpdatedAt.getTime());
+    });
+  });
+
+  describe("Likes", () => {
+    it("should add a like if no exists", () => {
+      const oldUpdatedAt = sut.updatedAt;
+      const likeId = Id.generate<"LikeId">();
+
+      vi.advanceTimersByTime(10);
+      sut.addLikeId(likeId);
+
+      expect(sut.likeIds).toContain(likeId);
+      expect(sut.updatedAt.getTime()).toBeGreaterThan(oldUpdatedAt.getTime());
+    });
+
+    it("shouldn't add a like if it exists", () => {
+      const oldUpdatedAt = sut.updatedAt;
+      const likeId = Id.generate<"LikeId">();
+
+      vi.advanceTimersByTime(10);
+      sut.addLikeId(likeId);
+
+      expect(sut.likeIds.length).toBe(1);
+      expect(sut.likeIds).toContain(likeId);
+      expect(sut.updatedAt.getTime()).toBeGreaterThan(oldUpdatedAt.getTime());
+
+      const newOldUpdatedAt = sut.updatedAt;
+
+      vi.advanceTimersByTime(10);
+      sut.addLikeId(likeId);
+
+      expect(sut.likeIds.length).toBe(1);
+      expect(sut.updatedAt.getTime()).not.toBeGreaterThan(
+        newOldUpdatedAt.getTime(),
+      );
+    });
+
+    it("should remove a like if it exists", () => {
+      const oldUpdatedAt = sut.updatedAt;
+      const likeId = Id.generate<"LikeId">();
+
+      sut.addLikeId(likeId);
+      vi.advanceTimersByTime(10);
+      sut.removeLikeId(likeId);
+
+      expect(sut.likeIds).not.toContain(likeId);
+      expect(sut.updatedAt.getTime()).toBeGreaterThan(oldUpdatedAt.getTime());
+    });
+
+    it("shouldn't update updatedAt when removing a non-existent like", () => {
+      const oldUpdatedAt = sut.updatedAt;
+      const likeId = Id.generate<"LikeId">();
+
+      vi.advanceTimersByTime(10);
+      sut.removeLikeId(likeId);
+
+      expect(sut.updatedAt.getTime()).not.toBeGreaterThan(
+        oldUpdatedAt.getTime(),
+      );
+    });
+  });
+
+  describe("Reply", () => {
+    it("should add a reply if no exists", () => {
+      const oldUpdatedAt = sut.updatedAt;
+      const replyId = Id.generate<"CommentId">();
+
+      vi.advanceTimersByTime(10);
+      sut.addReplyId(replyId);
+
+      expect(sut.replyIds).toContain(replyId);
+      expect(sut.updatedAt.getTime()).toBeGreaterThan(oldUpdatedAt.getTime());
+    });
+
+    it("shouldn't add a reply if it exists", () => {
+      const oldUpdatedAt = sut.updatedAt;
+      const replyId = Id.generate<"CommentId">();
+
+      vi.advanceTimersByTime(10);
+      sut.addReplyId(replyId);
+
+      expect(sut.replyIds.length).toBe(1);
+      expect(sut.replyIds).toContain(replyId);
+      expect(sut.updatedAt.getTime()).toBeGreaterThan(oldUpdatedAt.getTime());
+
+      vi.advanceTimersByTime(10);
+      sut.addReplyId(replyId);
+
+      const newOldUpdatedAt = sut.updatedAt;
+
+      expect(sut.replyIds.length).toBe(1);
+      expect(sut.updatedAt.getTime()).not.toBeGreaterThan(
+        newOldUpdatedAt.getTime(),
+      );
+    });
+
+    it("should remove a reply if it exists", () => {
+      const oldUpdatedAt = sut.updatedAt;
+      const replyId = Id.generate<"CommentId">();
+
+      sut.addReplyId(replyId);
+      vi.advanceTimersByTime(10);
+      sut.removeReplyId(replyId);
+
+      expect(sut.replyIds).not.toContain(replyId);
+      expect(sut.updatedAt.getTime()).toBeGreaterThan(oldUpdatedAt.getTime());
+    });
+
+    it("shouldn't update updatedAt when removing a non-existent reply", () => {
+      const oldUpdatedAt = sut.updatedAt;
+      const replyId = Id.generate<"CommentId">();
+
+      vi.advanceTimersByTime(10);
+      sut.removeReplyId(replyId);
+
+      expect(sut.updatedAt.getTime()).not.toBeGreaterThan(
+        oldUpdatedAt.getTime(),
+      );
+    });
+  });
+
+  describe("CreatedAt and UpdatedAt", () => {
+    it("should create a Comment with createdAt", () => {
+      const createdAt = new Date();
+      vi.advanceTimersByTime(1000);
+      const updatedAt = new Date();
+
+      const sut = makeComment(createdAt, updatedAt);
+
+      expect(sut.createdAt.getTime()).toBe(createdAt.getTime());
+      expect(sut.updatedAt.getTime()).toBe(updatedAt.getTime());
+    });
+  });
+});
