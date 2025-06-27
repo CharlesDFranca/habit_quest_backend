ccd61efdc8739922edbb048f338d6959acad6889
CharlesDFranca <josecharlestill@gmail.com>
2025-06-27 15:24:43 -0300

    refactoring of interfaces to leave id to be ignored as optional

--- src/modules/users/domain/services/interfaces/IEnsureAliasIsUniqueService.ts
+++ src/modules/users/domain/services/interfaces/IEnsureAliasIsUniqueService.ts
@@ -2,5 +2,8 @@ import { Alias } from "@/shared/domain/value-objects/Alias";
 import { Id } from "@/shared/domain/value-objects/Id";
 
 export interface IEnsureAliasIsUniqueService {
-  assertAliasIsUnique(alias: Alias, userId: Id<"UserId">): Promise<void>;
+  assertAliasIsUnique(
+    alias: Alias,
+    userIdToIgnore?: Id<"UserId">,
+  ): Promise<void>;
 }
--- src/modules/users/domain/services/interfaces/IEnsureEmailIsUniqueService.ts
+++ src/modules/users/domain/services/interfaces/IEnsureEmailIsUniqueService.ts
@@ -2,5 +2,8 @@ import { Id } from "@/shared/domain/value-objects/Id";
 import { Email } from "../../value-objects/Email";
 
 export interface IEnsureEmailIsUniqueService {
-  assertEmailIsUnique(email: Email, userId: Id<"UserId">): Promise<void>;
+  assertEmailIsUnique(
+    email: Email,
+    userIdToIgnore?: Id<"UserId">,
+  ): Promise<void>;
 }
