7f8b7904a4bc9f5b3ec92ab203bd5fb0eb24c8af
CharlesDFranca <josecharlestill@gmail.com>
2025-07-01 22:14:31 -0300

    refactoring 'FakeUserFactory' to be more versatile

--- tests/helpers/mocks/users/fakes/FakeUserFactory.ts
+++ tests/helpers/mocks/users/fakes/FakeUserFactory.ts
@@ -15,15 +15,26 @@ type FakeUserProps = {
 };
 
 export class FakeUserFactory {
-  static createUser(props: FakeUserProps, _id?: Id<"UserId">): User {
+  static createUser(props?: FakeUserProps, _id?: Id<"UserId">): User {
     const id = _id ?? Id.generate<"UserId">();
+
+    const userProps: FakeUserProps = {
+      alias: props && props.alias ? props?.alias : "user_alias",
+      email: props && props.email ? props?.email : "user@domain.com",
+      name: props && props.name ? props?.name : "user",
+      password: props && props.password ? props?.password : "Pass123!",
+      createdAt: props && props.createdAt ? props?.createdAt : new Date(),
+      updatedAt: props && props.updatedAt ? props?.updatedAt : new Date(),
+    };
 
     return User.create(
       {
-        name: Name.create({ value: props.name }),
-        alias: Alias.create({ value: props.alias }),
-        email: Email.create({ value: props.email }),
-        password: Password.create({ value: props.password }),
+        name: Name.create({ value: userProps.name }),
+        alias: Alias.create({ value: userProps.alias }),
+        email: Email.create({ value: userProps.email }),
+        password: Password.create({ value: userProps.password }),
+        createdAt: userProps.createdAt,
+        updatedAt: userProps.updatedAt,
       },
       id,
     );
