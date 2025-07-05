b37d4b634201393281befb1d55d7355195292d97
CharlesDFranca <josecharlestill@gmail.com>
2025-07-05 14:01:47 -0300

    implementation of the 'isInfraError' function to validate if the error is infra and addition of new codes in the 'HttpStatusCodeMapper'

--- src/shared/presentation/http/HttpErrorMapper.ts
+++ src/shared/presentation/http/HttpErrorMapper.ts
@@ -1,16 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { DomainError } from "@/shared/domain/errors/DomainError";
 import { ErrorResponse } from "./types/ErrorResponse";
 import { AppError } from "@/shared/app/errors/AppError";
 import { BaseError } from "@/shared/errors/BaseError";
+import { InfraError } from "@/shared/infra/errors/InfraError";
 
 export class HttpErrorMapper {
   static toErrorResponse(error: Error): ErrorResponse {
-    console.log("💥 Error caught:");
-    console.log("- name:", error.name);
-    console.log("- constructor.name:", error.constructor.name);
-    console.log("- instanceof DomainError:", error instanceof DomainError);
-    console.log("- instanceof AppError:", error instanceof AppError);
-
     if (isDomainError(error)) {
       return {
         code: (error as BaseError).code ?? "DOMAIN_ERROR",
@@ -27,10 +23,20 @@ export class HttpErrorMapper {
       };
     }
 
+    if (isInfraError(error)) {
+      return {
+        code: (error as BaseError).code ?? "INFRA_ERROR",
+        type: "Infrastructure",
+        message: error.message,
+        details: (error as any).original ?? undefined,
+      };
+    }
+
     return {
       code: "UNKNOWN_ERROR",
       type: "Internal",
       message: error.message ?? "Unexpected error occurred.",
+      details: (error as any).original ?? undefined,
     };
   }
 }
@@ -40,4 +46,8 @@ const isAppError = (error: Error): error is AppError => {
 };
 const isDomainError = (error: Error): error is DomainError => {
   return error.name === "DomainError" || error instanceof DomainError;
+};
+
+const isInfraError = (error: Error): error is InfraError => {
+  return error.name === "InfraError" || error instanceof InfraError;
 };
--- src/shared/presentation/http/HttpStatusCodeMapper.ts
+++ src/shared/presentation/http/HttpStatusCodeMapper.ts
@@ -6,6 +6,7 @@ export class HttpStatusCodeMapper {
       case ErrorCodes.EMAIL_ALREADY_USED:
       case ErrorCodes.ALIAS_ALREADY_USED:
       case ErrorCodes.ALREADY_LIKED:
+      case ErrorCodes.USER_ALREADY_BLOCKED:
         return 409;
 
       case ErrorCodes.NOT_FOUND:
@@ -13,11 +14,18 @@ export class HttpStatusCodeMapper {
 
       case ErrorCodes.INVALID_VALUE_OBJECT:
       case ErrorCodes.INVALID_ENTITY_TIMESTAMPS:
+      case ErrorCodes.EXCEEDED_POST_IMAGE_LIMIT:
+      case ErrorCodes.CANNOT_BLOCK_YOURSELF:
         return 422;
 
       case ErrorCodes.MISSING_REQUIRED_FIELDS:
       case ErrorCodes.MISSING_REQUIRED_PARAMETERS:
+      case ErrorCodes.INVALID_ENV_VARIABLE:
+      case ErrorCodes.MISSING_ENV_VARIABLE:
         return 400;
+
+      case ErrorCodes.BLOCK_USER_PERSISTENCE:
+        return 500;
 
       default:
         return 500;
